a7b21073735f924e34625735134f9d573c3b34c2
Arpan Mondal <arpan19991025@gmail.com>
2022-01-28 02:46:44 +0530

    add initial configs

---
+++ src/privateRoute/privateRoute.js
@@ -0,0 +1,8 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+export const PrivateRoute = ({ children }) => {
+
+    const { token } = useSelector((state) => state.auth);
+    return token ? children : <Navigate to="/login" />;
+};
